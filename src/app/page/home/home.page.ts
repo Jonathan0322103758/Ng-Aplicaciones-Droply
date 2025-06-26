2ca8428e86a5192cde395ce4c59b32afd56e3be8
Jonathan0322103758 <0322103758@ut-tijuana.edu.mx>
2025-06-25 22:54:50 -0700

    FEAT - componentes - checkbox

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { PreferenceComponent } from "@Component/feature/preference/preference.component";
 import { TitleHeaderComponent } from "@Component/shared/title-header/title-header.component";
+import { CheckboxComponent } from "@Component/UI/checkbox/checkbox.component";
 import { DropdownComponent } from "@Component/UI/dropdown/dropdown.component";
 import { InputComponent } from "@Component/UI/standalone";
 
 @Component({
     selector: 'page-home',
     standalone: true,
-    imports: [TitleHeaderComponent, PreferenceComponent, DropdownComponent, InputComponent],
+    imports: [TitleHeaderComponent, PreferenceComponent, DropdownComponent, InputComponent, CheckboxComponent],
     templateUrl: './home.page.html',
     styleUrl: './home.page.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
