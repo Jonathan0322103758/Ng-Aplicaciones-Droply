3787b877aac790128bf2501112876ea3010cf88d
Jonathan0322103758 <0322103758@ut-tijuana.edu.mx>
2025-06-27 22:10:00 -0700

    Fix - componentes UI standalone

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { PreferenceComponent } from "@Component/feature/preference/preference.component";
 import { TitleHeaderComponent } from "@Component/shared/title-header/title-header.component";
-import { InfoComponent } from "@Component/UI/standalone";
 
 @Component({
     selector: 'page-home',
     standalone: true,
-    imports: [TitleHeaderComponent, PreferenceComponent, InfoComponent],
+    imports: [TitleHeaderComponent, PreferenceComponent],
     templateUrl: './home.page.html',
     styleUrl: './home.page.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
