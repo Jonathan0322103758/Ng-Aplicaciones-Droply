28d669ad0809ef8dc1cf26ae241c89b92d33740e
Jonathan0322103758 <0322103758@ut-tijuana.edu.mx>
2025-06-28 23:33:17 -0700

    FEAT - modulo usuarios - asignar modulos

---
+++ src/app/components/feature/permisions/module-list/module-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, computed, inject, Signal } from '@angular/core';
+import { PreferenceService } from '@Client/preference/preference.service';
+import { ButtonComponent } from '@Component/UI/standalone';
+import { ColorType } from '@Types_/ui.types';
+
+@Component({
+  selector: 'qx-module-list',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './module-list.component.html',
+  styleUrl: './module-list.component.scss'
+})
+export class ModuleListComponent {
+  private readonly _preferenceService: PreferenceService = inject(PreferenceService);
+  public accentColor: Signal<ColorType> = computed(() => this._preferenceService.getPreference().color)
+
+  modules = [
+    { _id: 0, name: "Usuarios", icon: "user", status: false},
+    { _id: 0, name: "Notificaciones", icon: "bell", status: false},
+    { _id: 0, name: "Medidores", icon: "stopwatch",  status: false},
+    { _id: 0, name: "Lineas de Agua", icon: "droplet",  status: false},
+    { _id: 0, name: "Costos y Tarifas", icon: "chart-column",  status: false},
+    { _id: 0, name: "Actividades", icon: "screwdriver-wrench",  status: false},
+    { _id: 0, name: "Reportes", icon: "folder",  status: false},
+  ]
+
+  selectedModules: any[] = [];
+
+  sendModules(module: any, checked: boolean): void {
+  module.status = checked;
+
+  if (checked) {
+    if (!this.selectedModules.includes(module)) {
+      this.selectedModules.push(module);
+    }
+  } else {
+    this.selectedModules = this.selectedModules.filter(m => m !== module);
+  }
+}
+
+
+}
--- src/app/page/admin/users/users.page.ts
+++ src/app/page/admin/users/users.page.ts
@@ -1,17 +1,24 @@
 import { ChangeDetectionStrategy, Component, computed, inject, Signal } from "@angular/core";
 import { PreferenceService } from "@Client/preference/preference.service";
+import { ModuleListComponent } from "@Component/feature/permisions/module-list/module-list.component";
 import { UserListComponent } from "@Component/feature/users/user-list/user-list.component";
 import { TitleHeaderComponent } from "@Component/shared/title-header/title-header.component";
-import { ButtonComponent } from "@Component/UI/button/button.component";
-import { DropdownComponent } from "@Component/UI/dropdown/dropdown.component";
-import { InfoComponent, InputComponent } from "@Component/UI/standalone";
+import { ButtonComponent, DropdownComponent, InfoComponent, InputComponent } from "@Component/UI/standalone";
 import { Dropdown, Preference } from "@Interface/ui.interface";
 import { ButtonStyle } from "@Types_/ui.types";
 
 @Component({
     selector: 'page-users',
     standalone: true,
-    imports: [TitleHeaderComponent, ButtonComponent, InputComponent, DropdownComponent, InfoComponent, UserListComponent],
+    imports: [
+        TitleHeaderComponent,
+        ButtonComponent, 
+        InputComponent, 
+        DropdownComponent, 
+        InfoComponent, 
+        UserListComponent, 
+        ModuleListComponent
+    ],
     templateUrl: './users.page.html',
     styleUrl: './users.page.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
