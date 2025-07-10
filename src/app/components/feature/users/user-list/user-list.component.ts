86ff4123e68bd51358f835a3e5348ed6513e446c
Jonathan-Martinez <desarrollo.fullstack.2@rinorisk.com>
2025-07-10 08:26:59 -0700

    Merge - FEAT-modulo-usuarios - conflictos resueltos

--- src/app/components/feature/users/user-list/user-list.component.ts
+++ src/app/components/feature/users/user-list/user-list.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component, computed, inject, OnInit, signal, Signal } from '@angular/core';
-=======
 import { ChangeDetectionStrategy, Component, computed, inject, OnInit, output, signal, Signal } from '@angular/core';
->>>>>>> FEAT-modulo-usuarios
 import { TitleHeaderComponent } from '@Component/shared/title-header/title-header.component';
 import { UserCardSettingsComponent } from '../user-card-settings/user-card-settings.component';
 import { UserService } from '@Client/service/user.service';
@@ -12,41 +8,21 @@ import { PreferenceService } from '@Client/preference/preference.service';
 import { ColorType } from '@Types_/ui.types';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
-<<<<<<< HEAD
-=======
 import { InfoComponent } from '@Component/UI/standalone';
->>>>>>> FEAT-modulo-usuarios
 
 @Component({
   selector: 'qx-user-list',
   standalone: true,
-<<<<<<< HEAD
-  imports: [TitleHeaderComponent, UserCardSettingsComponent, FormsModule, AsyncPipe],
-  templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.scss'
-=======
   imports: [TitleHeaderComponent, UserCardSettingsComponent, FormsModule, AsyncPipe, InfoComponent],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
->>>>>>> FEAT-modulo-usuarios
 })
 export class UserListComponent implements OnInit {
   private readonly _preferenceService: PreferenceService = inject(PreferenceService);
   private readonly _userService: UserService = inject(UserService);
 
   public accentColor: Signal<ColorType> = computed(() => this._preferenceService.getPreference().color);
-<<<<<<< HEAD
-  public dummyUsers: User[] = this._userService.getDummyUsers();
-  public users$!: Observable<User[]>;
-  public users: Signal<User[]> = computed(() => this._userService.get())
-  public searchTerm = signal('');
-  public filteredUsers = computed(() => {
-  const term = this.searchTerm().toLowerCase();
-  return this.users().filter(user => user.name.toLowerCase().includes(term));
-});
-
-=======
   public users$!: Observable<User[]>;
   public users: Signal<User[]> = computed(() => this._userService.get())
   public searchTerm = signal<string>('');
@@ -68,7 +44,6 @@ public filteredUsers: Signal<User[]> = computed(() => {
     this.userSelected.emit(user);
   }
 
->>>>>>> FEAT-modulo-usuarios
   public ngOnInit(): void {
     this.users$ = this._userService.fetch();
   }
